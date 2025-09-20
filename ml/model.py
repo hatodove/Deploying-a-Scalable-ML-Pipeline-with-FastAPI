6e21efee0d656302ff66ab0bfe974f7af8d01624
Praise Sutton <psutto10@wgu.edu>
2025-09-20 23:50:52 +0900

    Implement model training, inference, save/load, and slice performance functions

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,9 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+import joblib
+import os
+from sklearn.linear_model import LogisticRegression
 
 
 # Optional: implement hyperparameter tuning.
@@ -19,8 +22,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = LogisticRegression(max_iter=2000, n_jobs=-1)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +63,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return preds
 
 
 def save_model(model, path):
@@ -73,14 +77,13 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    joblib.dump(model, path)
 
 
 def load_model(path):
     """Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    return joblib.load(path)
 
 
 def performance_on_categorical_slice(
@@ -120,14 +123,19 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    data_slice = data[data[column_name] == slice_value]
+
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column
         # given as "column_name", with the slice_value
-        # use training = False
+        data_slice,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = (
-        None  # your code here to get prediction on X_slice using the inference function
-    )
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
